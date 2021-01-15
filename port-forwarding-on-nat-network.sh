5aa4f963a391fa15cdb81260632fa556fc92a349
Sunjoo Park <allessunjoo.park@lge.com>
2021-01-15 15:34:25 +0900

    Udpate script for port-forward and running vms
    1. port-forwarding-on-nat-network.sh
        - Users only use rule name, forwarded host port / guest ip / guest
        port as arguments
        - A rule is generated  in a script
    2. running-vms-ip-info
        - Show network interface name

--- port-forwarding-on-nat-network.sh
+++ port-forwarding-on-nat-network.sh
@@ -2,18 +2,24 @@
 # Define port fowarding rule for NAT Network
 function usage() {
     echo "--nat-name NAT Network Name"
-    echo "--nat-rule NAT Port Forwarding Rule (including the rule name)"
-    echo "   > example ssh_1:tcp:[]:2010:[10.0.10.6]:22"
+    #echo "--nat-rule NAT Port Forwarding Rule (including the rule name)"
+    #echo "   > example ssh_1:tcp:[]:2010:[10.0.10.6]:22"
+    echo "--rule-name NAT Network Rule Name"
+    echo "--host-ip   Host IP for NAT Network Rule, default = ''"
+    echo "--host-port Host port for NAT Network"
+    echo "--guest-ip Guest OS IP on NAT Network"
+    echo "--guest-port Guest OS PORT on Guest IP"
 
     exit 2
 }
-ARGUMENTS=$(getopt -a -n alphabet -o n:r: --long nat-name:,nat-rule: -- "$@")
+ARGUMENTS=$(getopt -a -n alphabet -o n:r: --long nat-name:,rule-name:,host-ip:,host-port:,guest-ip:,guest-port: -- "$@")
 VALID_ARGUMENTS=$?
 if [ "$VALID_ARGUMENTS" != "0" ]; then
   usage
 fi
 echo "ARGUMENTS is $ARGUMENTS"
 eval set -- "$ARGUMENTS"
+FORWARD_PROTOCOL="tcp"
 while :
 do
   case "$1" in
@@ -24,8 +30,24 @@ do
         NAT_NAME="${2}"
         shift 2
         ;;
-    --nat-rule)
-        NAT_RULE="${2}"
+    --rule-name)
+        RULE_NAME="${2}"
+        shift 2
+        ;;
+    --host-ip)
+        HOST_IP="${2}"
+        shift 2
+        ;;
+    --host-port)
+        HOST_PORT="${2}"
+        shift 2
+        ;;
+    --guest-ip)
+        GUEST_IP="${2}"
+        shift 2
+        ;;
+    --guest-port)
+        GUEST_PORT="${2}"
         shift 2
         ;;
     --) shift; break ;;
@@ -33,7 +55,11 @@ do
        usage ;;
   esac
 done
-
+if [ -z "${HOST_IP}" ]; then
+  HOST_IP=""
+fi
+NAT_RULE="${RULE_NAME}:${FORWARD_PROTOCOL}:[${HOST_IP}]:${HOST_PORT}:[${GUEST_IP}]:${GUEST_PORT}"
+echo "NAT Network = ${NAT_NAME}"
+echo "NAT Rule = ${NAT_RULE}"
 VBoxManage natnetwork modify \
   --netname $NAT_NAME --port-forward-4 "$NAT_RULE"
-
